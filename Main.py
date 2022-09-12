25622e4de389320577c7a7ed637d0d822667ce6d
Chris <91715372+chris-x0@users.noreply.github.com>
2022-09-12 14:55:46 +0530

    second commit

--- Main.py
+++ Main.py
@@ -6,7 +6,7 @@ def selectionSort(array, size) -> List[int]:
     min_idx = step
     for i in range(step + 1, size):
       if array[i] < array[min_idx]:
-        min_idx = 1
+        min_idx = i
     (array[step], array[min_idx]) = (array[min_idx], array[step])
   return array
 
